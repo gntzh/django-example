181dc9aa7775fbb92a64f86aeef83e7943c21a5a
zure <1226968468z@gmail.com>
2018-01-28 01:18:51 +0800

    week1.2

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,35 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog, Category
 from django.views import generic
 from django.core.paginator import Paginator
+from django.conf import settings
 
 
 def blog_list(request):
     blogs_all = Blog.objects.all()
-    paginator = Paginator(blogs_all, 10)
+    paginator = Paginator(blogs_all, settings.PAGINATOR_PER_PAGE)  # 分页
     page_num = request.GET.get('page', 1)  # 获取页面参数(GET)
     page = paginator.get_page(page_num)
+    current_page_num = page.number
+    # 获取5页循环范围
+    page_range = list(
+        range(
+            max(current_page_num - 2, 1),
+            min(paginator.num_pages + 1, current_page_num + 3)))
+    # 加上省略标记...
+    if current_page_num >= 5:
+        page_range.insert(0, '...')
+    if current_page_num <= paginator.num_pages - 4:
+        page_range.append('...')
+    # 加上首页和尾页
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
     context = {}
     context['page'] = page
     context['categories'] = Category.objects.all()
+    context['page_range'] = page_range
     return render(request, 'blog/blog_list.html', context)
 
 
@@ -22,9 +41,31 @@ def blog_detail(request, blog_pk):
 
 
 def blog_with_category(request, category_pk):
-    context = {}
     category = get_object_or_404(Category, pk=category_pk)
-    context['blogs'] = Blog.objects.filter(category=category)
+    blogs_all = Blog.objects.filter(category=category)
+    paginator = Paginator(blogs_all, settings.PAGINATOR_PER_PAGE)  # 分页
+    page_num = request.GET.get('page', 1)  # 获取页面参数(GET)
+    page = paginator.get_page(page_num)
+    current_page_num = page.number
+    # 获取5页循环范围
+    page_range = list(
+        range(
+            max(current_page_num - 2, 1),
+            min(paginator.num_pages + 1, current_page_num + 3)))
+    # 加上省略标记...
+    if current_page_num >= 5:
+        page_range.insert(0, '...')
+    if current_page_num <= paginator.num_pages - 4:
+        page_range.append('...')
+    # 加上首页和尾页
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+    context = {}
+    context['page'] = page
     context['category'] = category
     context['categories'] = Category.objects.all()
+    context['page_range'] = page_range
     return render(request, 'blog/blog_with_category.html', context)
