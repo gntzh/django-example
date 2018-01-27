1e51f36144adeaef3e43710a5ccd104c98243d3a
zure <1226968468z@gmail.com>
2018-01-27 22:38:54 +0800

    week1.1.7

--- blog/models.py
+++ blog/models.py
@@ -20,12 +20,26 @@ class Tag(models.Model):
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
-    content = models.TextField()
-    created_time = models.DateTimeField(auto_now_add=True)
-    modified_time = models.DateTimeField(auto_now=True)
+    title = models.CharField('标题', max_length=50)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        verbose_name='作者',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.DO_NOTHING,
+        verbose_name='分类',
+    )
+    content = models.TextField('内容')
+    created_time = models.DateTimeField('发布时间', auto_now_add=True)
+    modified_time = models.DateTimeField('最近修改', auto_now=True)
+
+    class Meta:
+        ordering = [
+            '-modified_time',
+            '-created_time',
+        ]
 
     def __str__(self):
         return "<Blog: %s>" % self.title
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog, Category
 from django.views import generic
+from django.core.paginator import Paginator
 
 
 def blog_list(request):
+    blogs_all = Blog.objects.all()
+    paginator = Paginator(blogs_all, 10)
+    page_num = request.GET.get('page', 1)  # 获取页面参数(GET)
+    page = paginator.get_page(page_num)
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page'] = page
     context['categories'] = Category.objects.all()
     return render(request, 'blog/blog_list.html', context)
 
