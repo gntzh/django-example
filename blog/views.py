2764c2d328453ed59e83c16101b2ed022148bd60
zure-pc <1226968468@qq.com>
2018-02-07 14:51:14 +0800

    week2.3.1 不要使用特殊变量名,如list引发错误

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,15 @@ from django.core.paginator import Paginator
 from django.conf import settings
 
 import markdown
+md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ])
+    
 
-
-def blog_list(request):
-    blogs_all = Blog.objects.all()
-    paginator = Paginator(blogs_all, settings.PAGINATOR_PER_PAGE)  # 分页
+def my_paginator(request, iterable, per_page_num=settings.PAGINATOR_PER_PAGE):
+    paginator = Paginator(iterable, per_page_num) # 分页
     page_num = request.GET.get('page', 1)  # 获取页面参数(GET)
     page = paginator.get_page(page_num)
     current_page_num = page.number
@@ -31,50 +35,25 @@ def blog_list(request):
 
     context = {}
     context['page'] = page
-    context['categories'] = Category.objects.all()
     context['page_range'] = page_range
+
+    return context
+
+def blog_list(request):
+    blogs_all = Blog.objects.all()
+
+    context = my_paginator(request, blogs_all)
+    context['categories'] = Category.objects.all()
     return render(request, 'blog/blog_list.html', context)
-
-
-def blog_detail(request, blog_pk):
-    blog = get_object_or_404(Blog, pk=blog_pk)
-    context = {}
-    context['previous_blog'] = Blog.objects.filter(
-        modified_time__gt=blog.modified_time).last()
-    context['next_blog'] = Blog.objects.filter(
-        modified_time__lt=blog.modified_time).first()
-    context['blog'] = get_object_or_404(Blog, pk=blog_pk)
-    return render(request, 'blog/blog_detail.html', context)
 
 
 def blog_with_category(request, category_pk):
     category = get_object_or_404(Category, pk=category_pk)
     blogs_all = Blog.objects.filter(category=category)
-    paginator = Paginator(blogs_all, settings.PAGINATOR_PER_PAGE)  # 分页
-    page_num = request.GET.get('page', 1)  # 获取页面参数(GET)
-    page = paginator.get_page(page_num)
-    current_page_num = page.number
-    # 获取5页循环范围
-    page_range = list(
-        range(
-            max(current_page_num - 2, 1),
-            min(paginator.num_pages + 1, current_page_num + 3)))
-    # 加上省略标记...
-    if current_page_num >= 5:
-        page_range.insert(0, '...')
-    if current_page_num <= paginator.num_pages - 4:
-        page_range.append('...')
-    # 加上首页和尾页
-    if page_range[0] != 1:
-        page_range.insert(0, 1)
-    if page_range[-1] != paginator.num_pages:
-        page_range.append(paginator.num_pages)
-
-    context = {}
-    context['page'] = page
+    
+    context = my_paginator(request, blogs_all)
     context['category'] = category
     context['categories'] = Category.objects.all()
-    context['page_range'] = page_range
     return render(request, 'blog/blog_with_category.html', context)
 
 
@@ -98,13 +77,23 @@ class BlogAllFeed(Feed):
 '''
 
 
+def blog_detail(request, blog_pk):
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    blog.content = md.convert(blog.content)
+    blog.toc = md.toc
+
+    context = {}
+    context['previous_blog'] = Blog.objects.filter(
+        modified_time__gt=blog.modified_time).last()
+    context['next_blog'] = Blog.objects.filter(
+        modified_time__lt=blog.modified_time).first()
+    context['blog'] = blog
+    return render(request, 'blog/blog_detail.html', context)
+
+
 def blog_detail_md(request, blog_pk):
     blog = get_object_or_404(Blog, pk=blog_pk)
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        'markdown.extensions.toc',
-    ])
+    
     blog.content_md = md.convert(blog.content)
     blog.toc_md = md.toc
     context = {}
